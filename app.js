32e50e90ce503a112d9cb8b66737617f67518b45
samedyhunx <samedy.hunx@gmail.com>
2025-09-24 21:20:59 +0700

    master: nodejs project

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use(express.static(`${__dirname}/public`));
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
