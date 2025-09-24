c2184c0aa0ef27106f9db7eba9e1afb5c5956e96
samedyhunx <samedy.hunx@gmail.com>
2025-09-24 22:46:38 +0700

    master: nodejs project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,6 +21,8 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
     }
 
     // EXECUTE QUERY
