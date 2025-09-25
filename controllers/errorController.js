ca0383e5a6422d15550ceca90fb3ae12712b505e
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 15:14:47 +0700

    master: nodejs project

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDb = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDb = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -51,6 +57,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === "CastError") error = handleCastErrorDb(error);
     if (error.code === 11000) error = handleDuplicateFieldsDb(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDb(error);
 
     sendErrorProd(error, res);
   }
