6fa01856f97b1246a7eb21008c1b04ef6b0950f5
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 17:58:47 +0700

    master: nodejs project

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,6 +102,18 @@ exports.restrictTo = (...roles) => {
   };
 };
 
-exports.forgotPassword = (req, res, next) => {};
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1. Get user based on Posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError("There is no user with such email address", 404));
+  }
+
+  // 2. Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3. Send it to user's email
+});
 
 exports.resetPassword = (req, res, next) => {};
