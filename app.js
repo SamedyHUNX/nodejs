093d5762b033516eb0b8c2caa12dee8bda048dc3
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 21:57:50 +0700

    master: nodejs project

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const AppError = require("./utils/appError");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const globalErrorHandler = require("./controllers/errorController");
@@ -26,8 +29,13 @@ app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
