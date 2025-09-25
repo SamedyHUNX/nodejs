44317c2224472d0c0d6d6f138d7759d6986c33b5
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 21:50:26 +0700

    master: nodejs project

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const AppError = require("./utils/appError");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const globalErrorHandler = require("./controllers/errorController");
 const app = express();
 
@@ -11,7 +13,23 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+// Set security HTTP headers
+app.use(helmet());
+
+// Body parse, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+
+// Data sanitization against XSS
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
