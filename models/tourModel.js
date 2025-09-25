2cc601828c1d2c40969e2517af7497fae90c7c37
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 12:24:26 +0700

    master: nodejs project

--- models/tourModel.js
+++ models/tourModel.js
@@ -40,7 +40,13 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: function (val) {
+        return val < this.price;
+      },
+      message: "Discount price ({VALUE}) should be below regular price",
+    },
     summary: {
       type: String,
       trim: true,
