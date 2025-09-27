31519fec2e7f0718fceafc0e4c8dfec2119ac2d8
samedyhunx <samedy.hunx@gmail.com>
2025-09-27 15:27:46 +0700

    master: getOne handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,20 +40,7 @@ exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 
 // 4. Get a Tour by ID
-exports.getTourById = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate("reviews");
-
-  if (!tour) {
-    return next(new AppError("No tour found with that ID", 404));
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-});
+exports.getTourById = factory.getOne(Tour, { path: "reviews" });
 
 // 5. Delete a Tour
 exports.deleteTour = factory.deleteOne(Tour);
