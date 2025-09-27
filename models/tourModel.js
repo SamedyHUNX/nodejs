59a24c73e702b7d653dc3be56cea7582e686b438
samedyhunx <samedy.hunx@gmail.com>
2025-09-27 09:14:24 +0700

    master: nodejs project

--- models/tourModel.js
+++ models/tourModel.js
@@ -148,6 +148,14 @@ tourSchema.pre(/^find/, function (next) {
 //   this.find({ secretTour: { $ne: true } });
 // });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
+  next();
+});
+
 // AGGREGATION MIDDLEWARE
 tourSchema.pre("aggregate", function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
