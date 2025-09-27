b3c2469e4cd4737dfeaaf3902479c0bd3319bc98
samedyhunx <samedy.hunx@gmail.com>
2025-09-27 15:22:26 +0700

    master: createOne handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -15,6 +15,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
 exports.createReview = catchAsync(async (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,6 +3,7 @@ const {
   createReview,
   deleteReview,
   updateReview,
+  setTourUserIds,
 } = require("./../controllers/reviewController");
 const { protect, restrictTo } = require("./../controllers/authController");
 const express = require("express");
@@ -12,7 +13,7 @@ const router = express.Router({ mergeParams: true });
 router
   .route("/")
   .get(getAllReviews)
-  .post(protect, restrictTo("user"), createReview);
+  .post(protect, restrictTo("user"), setTourUserIds, createReview);
 
 router.route("/:id").patch(updateReview).delete(deleteReview);
 
