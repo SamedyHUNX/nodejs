e914c9f3c5f2256f29594e7e79178ad37d7b377f
samedyhunx <samedy.hunx@gmail.com>
2025-09-27 15:50:09 +0700

    master: getMe handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,16 +21,16 @@ const {
 
 const router = express.Router();
 
-router.get("/me", protect, getMe, getUser);
-
 // AUTHENTICATION
 router.post("/signin", login);
 router.post("/signup", signup);
 
 router.post("/forgot-password", forgotPassword);
 router.patch("/reset-password/:token", resetPassword);
-router.patch("/update-password/", protect, updatePassword);
 
+router.use(protect);
+router.get("/me", protect, getMe, getUser);
+router.patch("/update-password/", protect, updatePassword);
 router.patch("/update-me", protect, updateMe);
 router.delete("/delete-me", protect, deleteMe);
 
